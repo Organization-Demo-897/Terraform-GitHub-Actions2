2ad7efbb029e9db5032d6a0d8333f4685edfa03e
deepthakar <43946394+deepthakar@users.noreply.github.com>
2024-04-26 04:59:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-east-1"  # Change the region if needed
 }
 
+backend "s3" {
+  bucket         	   = "terraform-github-actions-demo-8973453"
+  key              	 = "state/terraform.tfstate"
+  region         	   = "us-east-1"
+  encrypt        	   = true
+  dynamodb_table     = "terraform-github-actions-db"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0f496107db66676ff"  # Specify your desired AMI ID
   instance_type = "t2.micro"                # Specify your desired instance type
